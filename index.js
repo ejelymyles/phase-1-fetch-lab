3baa5765992fe22d14b5063484271704b3792a33
Ej Myles <ejelymyles@gmail.com>
2023-05-15 23:06:26 -0700

    game of thrones fetch

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 function fetchBooks() {
+   return fetch('https://anapioficeandfire.com/api/books')
+  .then ((resp) => resp.json())
+  .then ((data) => renderBooks(data));
+
   // To pass the tests, don't forget to return your fetch!
   
 }
